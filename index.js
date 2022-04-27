5258c58b43b78ae502a0da7f71ceb15432ea3699
Sunless9999 <meritahakicigehingen@gmail.com>
2022-04-27 21:37:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
